09530ac323a4c30aca92010730aa61db4c9ae841
Tyheman <43628985+Tyheman@users.noreply.github.com>
2020-02-25 13:21:21 -0500

    Add files via upload

---
+++ TelephoneTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+
+public class TelephoneTest {
+
+    //All of the following tests can be used for the unformat method in the Telephone class
+
+    String number = "(570)345-1343";
+    @Test
+    public void isNumberEmpty() {
+        assertFalse(number.isEmpty(), () -> "Aborting test: Number is empty");
+    }
+
+    @Test
+    public void isNumberFormatted() {
+        assumeTrue(Telephone.isFormatted(number));
+    }
+
+    @Test
+    public void containsNoExtraCharacters(){
+        assertEquals(13, number.length());
+    }
+
+
+
+}
